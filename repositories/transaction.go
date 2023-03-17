29846d9dc6da3b80bf862590e378f97bcacd8012
achyarrbagus <achyarbagus@gmail.com>
2023-03-17 11:43:21 +0700

    m

--- repositories/transaction.go
+++ repositories/transaction.go
@@ -103,7 +103,7 @@ func (r *repository) UpdateTransaction(status string, ID int) (models.Transactio
 		}
 	}
 	transaction.Status = status
-	err := r.db.Exec("UPDATE 'transactions' SET 'status' = ?, WHERE id= ?", status, transaction.ID).Error
+	err := r.db.Exec("UPDATE transactions SET status = ? WHERE id= ?", status, transaction.ID).Error
 
 	// err := r.db.Model(&transaction).Where("id=?", transaction.ID).Update("status", status).Error
 
